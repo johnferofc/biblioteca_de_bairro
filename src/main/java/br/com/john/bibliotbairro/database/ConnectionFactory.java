a384f1f3bc8ea63bcc54e00538d4d4193931f8ac
johnferofc <johnfrcosta@gmail.com>
2025-08-12 11:48:12 -0300

    Atualização de credenciais.

--- src/main/java/br/com/john/bibliotbairro/database/ConnectionFactory.java
+++ src/main/java/br/com/john/bibliotbairro/database/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String URL = "jdbc:mysql://localhost/database";
     private static final String USER = "root";
     private static final String PASSWORD = "sua_senha_aqui";
 
